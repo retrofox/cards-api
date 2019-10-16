38f52132f601b20f46277c8d165712a4a9a8a674
retrofox <rdsuarez@gmail.com>
2019-10-16 19:02:00 -0300

    add meta endpoint to `/cards` endpoint

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -2,8 +2,21 @@ import { getCardsListObject } from '../../components/cards-list';
 import { map } from 'lodash';
 
 export default async ( req, res ) => {
-  res.status( 200 ).json( getCardsListObject( {
-    host: req.headers.host,
+  const host = req.headers.host;
+
+  const cards = getCardsListObject( {
+    host,
     fileExt: req.query.fileExt || null,
-  } ) );
+  } );
+
+  const response = map( cards, ( card ) => Object.assign(
+    card,
+    {
+      meta: {
+        endpoint: `http://${ host }/api/cards/${ card.id }`,
+      }
+    }
+  ) );
+
+  res.status( 200 ).json( response );
 };
